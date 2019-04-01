9633af34e4e1787151404d35ec70b710017f8211
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-03-31 21:42:26 -0500

    setup

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,19 +3,30 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "RootRoot",
   database: "bamazonDB"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+  console.log("Welcome, connected as id " + connection.threadId + "\n");
+  Start();
 });
+
+// function start() {}
+// show inventory
+// prompt the customer with two questions
+inquirer.prompt([
+  {
+    type: "number",
+    message: "Please enter the ID of the item that you would like to purchase:",
+    name: "id"
+  },
+  {
+    type: "number",
+    message: "Please enter the quantity:",
+    name: "quantity"
+  }
+]);
