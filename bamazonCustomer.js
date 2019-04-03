af1bcf03123f2a4eb43bccc2e0c6b6edcb6ce7b4
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-04-02 23:48:51 -0500

    cust added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,24 +21,40 @@ connection.connect(function(err) {
 function startCustomer() {
   connection.query("SELECT * FROM products", function(err, result) {
     if (err) throw err;
-    console.log("Welcome to BAMazon! On sale today:");
+    console.log("Welcome to Bamazon! On sale today:");
     console.table(result);
     sale();
   });
 }
 
 function sale() {
-  inquirer.prompt([
-    {
-      type: "input",
-      message:
-        "Please enter the ID of the item that you would like to purchase:",
-      name: "id"
-    },
-    {
-      type: "input",
-      message: "Please enter the quantity:",
-      name: "quantity"
-    }
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "Please enter the ID of the item that you would like to purchase:",
+        name: "id"
+      },
+      {
+        type: "input",
+        message: "Please enter the quantity:",
+        name: "qty"
+      }
+    ])
+    .then(function(stockcheck) {
+      connection.query(
+        "SELECT * FROM products WHERE item_id=?",
+        stockcheck.id,
+        function(err, result) {
+          for (var i = 0; i < result.length; i++) {
+            if (stockcheck.qty > result[i].stock_quantity) {
+              console.log("Sorry, not enough in stock, please try again");
+            } else {
+              console.log("Added to cart");
+            }
+          }
+        }
+      );
+    });
 }
